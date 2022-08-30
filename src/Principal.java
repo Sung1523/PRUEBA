044bfae4934acf693bc73ae99032271d801f209f
SAMANTHA <samantha.ramos@pucp.edu.pe>
2022-08-30 12:17:43 -0500

    nuevo

---
+++ src/Principal.java
@@ -0,0 +1,9 @@
+public class Principal {
+    public static void main(String[] args) {
+        String saludo = "hola";
+        System.out.println("HELLO lasla :)");
+        System.out.println("Hello there");
+        System.out.println(saludo);
+        System.out.println(":o");
+    }
+}
